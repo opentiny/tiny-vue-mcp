2df5fc6188b7c39b60fd0edb0c6aeeb69f87613b
ajaxzheng <894103554@qq.com>
2025-06-02 16:46:55 +0800

    feat: 调通AI对接mcp工具

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -1,8 +1,42 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import * as Vue3 from "vue";
+import { createI18n } from "vue-i18n";
+import { initI18n, t } from "@opentiny/vue-locale";
+import router from "./router";
+import App from "./App.vue";
+import { registerMcpConfig } from "@opentiny/vue-common";
+import {
+  createMcpTools,
+  getTinyVueMcpConfig,
+  zhCN,
+  enUS,
+} from "@opentiny/tiny-vue-mcp";
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = Vue3.createApp(App);
+
+const mode = location.pathname.split("/")[1] || "pc";
+
+app.config.globalProperties.tiny_mode = { value: mode };
+app.config.globalProperties.isPcMode = mode === "pc";
+app.config.globalProperties.isMobileMode = mode === "mobile";
+
+// 注入全局的saas主题变量
+app.config.globalProperties.tiny_theme = {
+  value: import.meta.env.VITE_TINY_THEME,
+};
+
+app.use(
+  initI18n({
+    createI18n,
+    i18n: {},
+    messages: {
+      zhCN,
+      enUS,
+    },
+  } as any)
+);
+
+// 注册TinyVue组件mcp配置
+registerMcpConfig(getTinyVueMcpConfig({ t }), createMcpTools);
+
+app.use(router);
+app.mount("#app");
