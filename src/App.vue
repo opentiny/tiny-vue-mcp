aaefd2857af9d6263813dcbd534386cbe00f97f1
shenjunjian <shenjunjian5@huawei.com>
2025-06-11 15:39:03 +0800

    fix(step4): fix

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <div class="qr-code">
+    <span>页面识别二维码， 请用手机扫码后，在浏览器中打开。 微信的浏览器可能没有语音功能，需要使用手机内置的浏览器访问。 </span>
+    <tiny-qr-code
+    :value="sessionUrl"
+    :size="100"
+    color="#1677ff"
+    ></tiny-qr-code>
+  </div>
   <div class="app-container">
     <!-- 主体内容区域 -->
     <div class="main-content">
@@ -21,6 +29,8 @@ import { showTinyRobot } from "./composable/utils";
 import { IconAi } from "@opentiny/tiny-robot-svgs";
 import { useNextClient } from "@opentiny/next-vue";
 import { globalConversation } from "./composable/utils";
+import { computed } from "vue";
+import CryptoJS from "crypto-js";
 
 // 2、创建 sessionId , 一个 ref<string> 的值, 并保存起来，智能助手会访问该变量进行通讯
 const { sessionId } = useNextClient({
@@ -29,6 +39,16 @@ const { sessionId } = useNextClient({
 });
 
 globalConversation.sessionId = sessionId;
+
+const sessionUrl = computed(() => {
+  // 未创建session时
+  if (!sessionId.value) return "no session";
+
+  // 创建好 sessionId后，加密后拼接成 sessionUrl， 这样手机扫码后会连接这个Url页面
+  const encryptedId = CryptoJS.AES.encrypt(sessionId.value, "secret-session-id" ).toString();
+
+  return "http://39.108.160.245?id=" + encodeURIComponent(encryptedId);
+});
 </script>
 
 <style scoped>
@@ -42,7 +62,9 @@ globalConversation.sessionId = sessionId;
 }
 
 .qr-code {
-  margin-right: 20px;
+  margin: 20px;
+  display: flex;
+  align-items: baseline;
 }
 
 .app-container {
